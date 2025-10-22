83cdc5570d0693d96019e37cab256305ce346faa
taeseonYoo <jkl0642@hansung.ac.kr>
2025-10-22 20:22:18 +0900

    feat(Input): 사용자의 입력을 받는 기능 구현
    - 경주할 자동차 이름 입력 기능
    - 시도할 횟수 입력 기능

---
+++ src/main/java/racingcar/view/Input.java
@@ -0,0 +1,18 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+    private final static String CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private final static String ATTEMPTS_MESSAGE = "시도할 횟수는 몇 회인가요?";
+
+    public String inputCarNames() {
+        System.out.println(CAR_NAMES_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String inputAttempts() {
+        System.out.println(ATTEMPTS_MESSAGE);
+        return Console.readLine();
+    }
+}
