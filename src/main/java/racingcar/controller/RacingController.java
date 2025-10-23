523a6905ff08f48a5c8b31bab0cbb056a3e87c15
taeseonYoo <jkl0642@hansung.ac.kr>
2025-10-23 18:47:01 +0900

    refactor(RacingController): 자동차, 시도 횟수 로직을 리팩토링
    - 자동차를 세팅하는 메서드로 분리했다
    - 시도 횟수를 세팅하는 메서드로 분리했다

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,6 +1,7 @@
 package racingcar.controller;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import racingcar.model.Car;
@@ -14,20 +15,38 @@ public class RacingController {
         Input input = new Input();
         Output output = new Output();
 
-        String inputCarNames = input.inputCarNames();
-        InputValidator.validateCarNames(inputCarNames);
+        List<Car> cars = settingCars(input.inputCarNames());
+        int attempts = settingAttempts(input.inputAttempts());
 
-        String[] carNames = Separator.splitCarNames(inputCarNames);
-        List<Car> cars = Arrays.stream(carNames)
-                .map(Car::new).toList();
-
-        String attempts = input.inputAttempts();
-
-        //게임을 시작한다.
         output.printRaceResult();
-
-        output.printRoundResult(cars);
+        for (int i = 0; i < attempts; i++) {
+            for (int j = 0; j < cars.size(); j++) {
+                int randomNum = Randoms.pickNumberInRange(0, 9);
+                cars.get(j).work(randomNum);
+            }
+            output.printRoundResult(cars);
+        }
+        //게임 끝 승자를 집계한다.
+        List<Car> winners = new ArrayList<>();
+        cars.sort(Car::compareTo);
 //        output.printResult();
+    }
+
+    private static List<Car> settingCars(String inputCarNames) {
+        InputValidator.validateCarNames(inputCarNames);
+        String[] carNames = Separator.splitCarNames(inputCarNames);
+        return Arrays.stream(carNames)
+                .map(Car::new).toList();
+    }
+
+    private static int settingAttempts(String inputAttempts) {
+        try {
+            int attempts = Integer.parseInt(inputAttempts);
+            InputValidator.validateAttempts(attempts);
+            return attempts;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("시도 횟수는 int 범위 안의 양의 정수를 입력해야 합니다.");
+        }
     }
 
 }
