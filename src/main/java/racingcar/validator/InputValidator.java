ca738c18858bb955935948332fda0b802490e5d1
taeseonYoo <jkl0642@hansung.ac.kr>
2025-10-23 22:13:01 +0900

    fix(InputValidator): 시도 횟수가 0인 경우 예외가 발생하지않는 버그 수정
    - 시도 횟수 검증 시 0으로 시작하는 경우를 검증할 수 없어 문제가 발생함

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -4,9 +4,9 @@ import java.util.regex.Pattern;
 
 public class InputValidator {
     private final static String VALID_CAR_NAME_REGEX = "^[a-zA-Z,]*$";
-    private final static String ONLY_NUMBER_REGEX = "^[0-9]+$";
+    private final static String ONLY_NUMBER_REGEX = "^[1-9][0-9]+$";
     private final static String CAR_NAME_INPUT_EXCEPTION = "알파벳과 (,)만 입력해주세요.";
-    private final static String ATTEMPTS_INPUT_EXCEPTION = "시도 횟수는 숫자만 입력 가능합니다.";
+    private final static String ATTEMPTS_INPUT_EXCEPTION = "시도 횟수는 0 이상의 숫자만 입력 가능합니다.";
 
     public static void validateCarNames(String inputCarNames) {
         if (!Pattern.matches(VALID_CAR_NAME_REGEX, inputCarNames)) {
