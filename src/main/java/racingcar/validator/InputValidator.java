2e5897c1338b192778de8376a2c7fbebefcf8a43
taeseonYoo <jkl0642@hansung.ac.kr>
2025-10-23 23:49:51 +0900

    fix(InputValidator): 한자리 숫자가 입력되지 않는 버그 수정

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class InputValidator {
     private final static String VALID_CAR_NAME_REGEX = "^[a-zA-Z,]*$";
-    private final static String ONLY_NUMBER_REGEX = "^[1-9][0-9]+$";
+    private final static String ONLY_NUMBER_REGEX = "^[1-9][0-9]*$";
     private final static String CAR_NAME_INPUT_EXCEPTION = "알파벳과 (,)만 입력해주세요.";
     private final static String ATTEMPTS_INPUT_EXCEPTION = "시도 횟수는 0 이상의 숫자만 입력 가능합니다.";
 
