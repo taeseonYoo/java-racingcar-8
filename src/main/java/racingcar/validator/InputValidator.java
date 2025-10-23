553797bca69bc48e6c3635830cdcb949060b52e5
taeseonYoo <jkl0642@hansung.ac.kr>
2025-10-23 18:49:14 +0900

    feat(InputValidator): 시도 횟수 검증 로직 구현

--- src/main/java/racingcar/validator/InputValidator.java
+++ src/main/java/racingcar/validator/InputValidator.java
@@ -4,9 +4,16 @@ import java.util.regex.Pattern;
 
 public class InputValidator {
     private final static String VALID_REGEX = "^[a-zA-Z,]*$";
+
     public static void validateCarNames(String inputCarNames) {
-        if (!Pattern.matches(VALID_REGEX,inputCarNames)) {
+        if (!Pattern.matches(VALID_REGEX, inputCarNames)) {
             throw new IllegalArgumentException("알파벳과 (,)만 입력해주세요");
+        }
+    }
+
+    public static void validateAttempts(int attempts) {
+        if (attempts < 1) {
+            throw new IllegalArgumentException("시도 횟수는 최소 1보다 큰 숫자여야합니다.");
         }
     }
 }
